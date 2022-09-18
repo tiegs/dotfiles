2fd6a334f8331a9bc5317b4819e8159e0e55a30d
Tilman Eggers <tilman.eggers@cgi.com>
2022-09-18 22:40:25 +0200

    Add tmux autostart to fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,17 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    /usr/bin/neofetch
+    #/usr/bin/neofetch
 
+    # Check if tmux is already running
+    if test -z $TMUX
+      /usr/bin/neofetch
+    else
+      # If not start tmux
+      test -z $TMUX; and exec tmux
+    end
+
+    #test -n $TMUX; and exec neofetch
+
+    # Automatically run tmux (as long as its not already running)
+    #test -z $TMUX; and exec tmux
 end
