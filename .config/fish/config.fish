b1442ca9796d967155b616d94a5557c37f6deac3
Tilman Eggers <tilman.eggers@cgi.com>
2022-09-21 13:15:03 +0200

    Update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,17 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    #/usr/bin/neofetch
 
-    # Check if tmux is already running
-    if test -z $TMUX
-      # If not start tmux
+    if [ "$TERM_PROGRAM" = "guake" ]
+      # Do not start tmux in guake shell
+      /usr/bin/neofetch
+    else if test -z "$TMUX"
+      # Start tmux if not alreay in tmux
       exec tmux
     else
+      # Runs _inside_ each tmux session
       /usr/bin/neofetch
     end
-
-    #test -n $TMUX; and exec neofetch
-
+      
     # Automatically run tmux (as long as its not already running)
-    #test -z $TMUX; and exec tmux
+    #test -z "$TMUX"; and exec tmux
 end
