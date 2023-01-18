ebb717cf6b71aa154b16de5f95ce32d1061fb6cb
Tilman Eggers <dev@tilmaneggers.de>
2023-01-18 20:07:57 +0100

    Remove non-eos files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,18 +1,3 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-
-    if [ "$TERM_PROGRAM" = "guake" ]
-      # Do not start tmux in guake shell
-      /usr/bin/neofetch
-    else if test -z "$TMUX"
-      # Start tmux if not alreay in tmux
-      exec tmux
-      #/usr/bin/neofetch
-    else
-      # Runs _inside_ each tmux session
-      /usr/bin/neofetch
-    end
-      
-    # Automatically run tmux (as long as its not already running)
-    #test -z "$TMUX"; and exec tmux
 end
