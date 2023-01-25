6e0dc51626c28acce2bd9adcc4801451f7f292d6
Tilman Eggers <dev@tilmaneggers.de>
2023-01-25 17:16:03 +0100

    feat(starship): Update prompt and add k8s config (for use with ENV-var)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,19 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    /usr/bin/neofetch
+
+    # Currently byobu is started by the terminal command (kitty byobu-tmux new)
+    #if [ "$TERM_PROGRAM" = "guake" ]
+    #  # No byoby in guake
+    #  /usr/bin/neofetch
+    #
+    #else if test -z "$TMUX"
+    #  # Start new byobu session per terminal that is not running a tmux session yet
+    #  byobu-tmux new
+    #else
+    #  # Run neofetch in each session
+    #  /usr/bin/neofetch
+    #end
+
 end
+#status --is-login; and status --is-interactive; and exec byobu-launcher
