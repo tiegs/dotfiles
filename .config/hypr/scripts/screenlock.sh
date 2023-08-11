2224005ddde126470c6af164cae23f91c2082d59
Tilman Eggers <dev@tilmaneggers.de>
2023-08-11 15:47:42 +0200

    feat(hyprland): Set up lockscreen

---
+++ .config/hypr/scripts/screenlock.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+swaylock \
+	--screenshots \
+	--clock \
+	--indicator \
+	--indicator-idle-visible \
+	--indicator-radius 100 \
+	--indicator-thickness 7 \
+	--effect-blur 7x5 \
+	--effect-vignette 0.5:0.5 \
+	--ring-color bb00cc \
+	--key-hl-color 880033 \
+	--line-color 00000000 \
+	--inside-color 00000088 \
+	--separator-color 00000000 \
+	--grace 2 \
+	--grace-no-mouse \
+	--grace-no-touch \
+	--fade-in 0.2
