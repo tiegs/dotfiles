ccfb38cdcf087c510971eaedf88db7d753ab4068
Tilman Eggers <dev@tilmaneggers.de>
2023-09-05 18:26:01 +0200

    feat(hyprland): Add tailscale module to waybar

---
+++ .config/waybar/scripts/tailscale.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+ts_bin="/usr/bin/tailscale"
+jq_bin="/usr/bin/jq -r"
+
+status=$($ts_bin status --self --peers=false --json)
+# echo $status
+
+svc_state=$(echo $status | $jq_bin ".BackendState")
+# echo $svc_state
+
+online=$(echo $status | $jq_bin ".Self.Online")
+# echo $conn_state
+
+tunneled=$(echo $status | $jq_bin ".ExitNodeStatus.Online")
+# echo $tunneled
+
+if [ "$svc_state" = "Running" ]; then
+	# echo "Service is running"
+	if [ $online = "true" ]; then
+		# echo "Service is connected"
+		if [ $tunneled = "true" ]; then
+			# echo "Service is tunneled"
+			alt="tunneled"
+		else
+			# echo "Service is not tunneled"
+			alt="connected"
+		fi
+	else
+		# echo "Service is disconnected"
+		alt="disconnected"
+	fi
+else
+	# echo "Service is stopped"
+	alt="stopped"
+fi
+
+echo "{\"text\": \"tailscale dummy text\", \"alt\": \"$alt\", \"tooltip\": \"tailscale tooltip $alt\", \"class\": \"$alt\"}"
--- .config/waybar/scripts/update.sh
+++ .config/waybar/scripts/update.sh
@@ -3,5 +3,8 @@
 # Run updates
 /usr/bin/eos-update
 
+# Update waybar
+pkill -RTMIN+1 waybar
+
 # Keep window open until any keypress
 read -p 'All done!'
