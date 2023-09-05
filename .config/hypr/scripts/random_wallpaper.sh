df7593959c287f7bd616b8b591c3ce71fdaca539
Tilman Eggers <dev@tilmaneggers.de>
2023-09-05 18:27:17 +0200

    feat(hyprland): Add random wallpaper script

--- .config/hypr/scripts/random_wallpaper.sh
+++ .config/hypr/scripts/random_wallpaper.sh
@@ -5,5 +5,14 @@ wp_dir=~/Pictures/Wallpapers/Tokyo\ Night/
 
 # params for swww that define the transition
 transition_wipe="-t wipe --transition-angle 30 --transition-step 100 --transition-fps 120 --transition-duration 1.5"
+transition_fast="-t simple --transition-step 25"
 
-find "$wp_dir" | shuf -n 1 | xargs -d '\n' swww img $transition_wipe
+case $1 in
+fast)
+	find "$wp_dir" | shuf -n 1 | xargs -d '\n' swww img $transition_fast
+	;;
+
+*)
+	find "$wp_dir" | shuf -n 1 | xargs -d '\n' swww img $transition_wipe
+	;;
+esac
